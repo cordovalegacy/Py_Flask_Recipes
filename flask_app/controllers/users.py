a205658a33747fc94b41539f369def9e9ac8cf08
Brendan Cordova <cordovalegacy19@gmail.com>
2023-02-22 10:30:09 -0800

    User Login and Registration w/ Validations Complete

---
+++ flask_app/controllers/users.py
@@ -0,0 +1,52 @@
+from flask_app import app
+from flask import render_template, redirect, request, session, flash
+from flask_app.models.user import User
+from flask_bcrypt import Bcrypt
+bcrypt = Bcrypt(app)
+
+@app.route('/')
+def home():
+    return render_template('registration.html')
+
+@app.route('/login_page')
+def login_page():
+    return render_template('login.html')
+
+@app.route('/register', methods = ['POST'])
+def register():
+    if not User.validate(request.form):
+        return redirect('/')
+    form_data = {
+        'first_name': request.form['first_name'], 
+        'last_name': request.form['last_name'], 
+        'email': request.form['email'], 
+        'password': bcrypt.generate_password_hash(request.form['password']) 
+    }
+    id = User.save_registration(form_data)
+    session['user_id'] = id
+    return redirect('/user_page')
+
+@app.route('/login', methods = ['POST'])
+def login():
+    logged_user = User.get_one_user_by_email(request.form)
+    if not logged_user:
+        flash("Invalid Email Address", 'Login')
+        return redirect('/login_page')
+    if not bcrypt.check_password_hash(logged_user.password, request.form['password']):
+        return redirect('/')
+    session['user_id'] = logged_user.id
+    return redirect('/user_page')
+
+@app.route('/user_page')
+def user_page():
+    if 'user_id' not in session:
+        return redirect('/logout')
+    data = {
+        'id': session['user_id']
+    }
+    return render_template('user_page.html', user = User.get_one_user_by_id(data))
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect('/')
---
+++ flask_app/models/user.py
@@ -0,0 +1,68 @@
+from flask_app.config.mysqlconnection import connectToMySQL
+from flask import flash
+import re
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$') 
+
+class User:
+
+    def __init__(self, data):
+        self.id = data['id']
+        self.first_name = data['first_name']
+        self.last_name = data['last_name']
+        self.email = data['email']
+        self.password = data['password']
+        self.created_at = data['created_at']
+        self.updated_at = data['updated_at']
+
+    @classmethod
+    def get_one_user_by_id(cls, data):
+        query = "SELECT * FROM users_table WHERE id=%(id)s"
+        result = connectToMySQL('recipes').query_db(query, data)
+        print(result[0])
+        return cls(result[0])
+
+    @classmethod
+    def get_one_user_by_email(cls, data):
+        query = "SELECT * FROM users_table WHERE email=%(email)s"
+        result = connectToMySQL('recipes').query_db(query, data)
+        if len(result) < 1:
+            return False
+        return cls(result[0])
+
+    @classmethod
+    def save_registration(cls, form_data):
+        query = """
+                INSERT INTO users_table (first_name, last_name, email, password, created_at, updated_at)
+                VALUES (%(first_name)s, %(last_name)s, %(email)s, %(password)s, NOW(), NOW())
+                ;"""
+        return connectToMySQL('recipes').query_db(query, form_data)
+    
+    @staticmethod
+    def validate(form_data):
+        is_valid = True
+        query = """
+                SELECT * FROM users_table 
+                WHERE email = %(email)s
+                ;"""
+        results = connectToMySQL('recipes').query_db(query, form_data)
+        if len(results) >=1:
+            flash("Email is taken, enter a different one", 'Register')
+            is_valid = False
+        if len(form_data['first_name']) < 2:
+            flash("First Name must be at least 2 characters", 'Register')
+            is_valid = False
+        if len(form_data['last_name']) < 2:
+            flash("Last Name must be at least 2 characters", 'Register')
+            is_valid = False
+        if not EMAIL_REGEX.match(form_data['email']):
+            flash("Please register with a valid email address", 'Register')
+            is_valid = False
+        if len(form_data['password']) < 7:
+            flash("Password must be at least 7 characters", 'Register')
+            is_valid = False
+        if form_data['password'] != form_data['confirm']:
+            flash("Passwords must match", 'Register')
+            is_valid = False
+        return is_valid
+
+
